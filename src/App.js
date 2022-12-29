80352eb0bec9955f3781ac3d07ea9fd04882d77e
oagomezz <ogomezbarragan437@gmail.com>
2022-12-29 12:43:34 -0800

    finished the first section of challenges

--- src/App.js
+++ src/App.js
@@ -3,12 +3,36 @@ import "./App.css"
 import LightSwitch from "./components/LightSwitch"
 
 const App = () => {
-  const [lightSwitchStatus, setLightSwitchStatus] = useState("off")
+  const [lightSwitchStatus, setLightSwitchStatus] = useState('off')
 
+  // This code block toggles the light switch. It changes the setLightSwitchStatus variable from on to off. 
+
+  const toggleLightSwitch = () => {
+    if(lightSwitchStatus === 'off') {setLightSwitchStatus('on')
+     } else if (lightSwitchStatus === 'on') {
+    setLightSwitchStatus('off')  
+     }
+    }
+
+
+
+  // Does not work on the style for the code. Can't get the bulb to switch colors from white when box is clicked.
+
+  // var bulbColor = () => {
+  //   if(lightSwitchStatus === 'off') {
+  //    return 'white'
+  //   } else if(lightSwitchStatus === 'on') {
+  //     return 'yellow'
+  //   }
+  // }
   return (
     <>
       <h1>Lightbulb Challenge</h1>
+      {/* <LightSwitch />
       <LightSwitch />
+      <LightSwitch /> */}
+      <LightSwitch toggleLightSwitch = {toggleLightSwitch} lightSwitchStatus = {lightSwitchStatus}/>
+      
     </>
   )
 }
